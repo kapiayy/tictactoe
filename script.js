3472316093fa556abee939a20ad767a2e1b33ce6
Kapi <kacperkozlowskii12@gmail.com>
2025-06-01 19:45:09 +0200

    Fix UI

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const gameBoard = (function () {
     const columns = 3
     const board = []
     let inc = 1
-    let currentPlayer = 'x'
+    
     for(let i = 0; i < rows; i++){
             board[i] = []
         for(let j = 0; j < columns; j++){
@@ -13,33 +13,26 @@ const gameBoard = (function () {
         
     }
     
+  
+
     const getBoard = () => board
     
-    const nextTurn = () => {
-        currentPlayer = currentPlayer === 'x' ? 'o' : 'x'
-    }
-    const getTurn = () => currentPlayer 
-    
-
-    const setValue = (cords) => {
+ 
+    const setValue = (cords,currentPlayer) => {
         
         const row = Math.floor((cords - 1) / columns)
         const col = (cords - 1) % columns
         if(typeof board[row][col] === 'string'){
             console.log("✖ This Spot is Taken")
-        }else{
-            board[row][col] = currentPlayer
-            nextTurn()
-            return getBoard()
+            return false
         }
+        else{
+            board[row][col] = currentPlayer.getPlayer().playerSign
+           return true
+            }
     }
-
     
-    
-    
-    
-    
-    return { getBoard, setValue, nextTurn, getTurn }
+    return { getBoard, setValue, }
     
 })()
 
@@ -60,11 +53,20 @@ function createPlayer(name,sign){ // funkcja tworzaca gracza
 }
 
 
-
  
 const game = (function gameController(){
+    const player1 = createPlayer(`kapi`,'x')
+    const player2 = createPlayer('niger','o')
+    currentPlayer = player1
+    sign = currentPlayer.getPlayer().playerSign
     
-    
+    function switchPlayers(){
+        
+        currentPlayer = currentPlayer === player1 ? player2 : player1
+        
+    }
+
+   
 
     function renderBoard(){
         return gameBoard.getBoard()
@@ -72,24 +74,104 @@ const game = (function gameController(){
     
 
     function move(cords){ 
-        gameBoard.setValue(cords)
+        const success = gameBoard.setValue(cords,currentPlayer)
+        if(!success){
+            return{error: 'Spot Taken', board: renderBoard(),turn: currentPlayer.getPlayer().playerSign, player: currentPlayer.getPlayer().playerName }
+        }else  if(checkWin()){
+            return { board: renderBoard(), winner: currentPlayer.getPlayer().playerName };
+        }else{
+        switchPlayers()
+        // if checkwin goes console log win if not the nswtich players
         
-        return{ board:gameBoard.getBoard(), nextTurn: gameBoard.getTurn()
+        return{ board:gameBoard.getBoard(),turn: currentPlayer.getPlayer().playerSign, player: currentPlayer.getPlayer().playerName
             
-                
+        }
+        
         }
     }
     
-
-    
-    
+    function checkWin() {
+        board = gameBoard.getBoard()
+        const sign = currentPlayer.getPlayer().playerSign
         
+        for(let i = 0 ; i < 3; i++){
+            if(board[i][0] === sign && board[i][1] === sign && board[i][2] === sign){
+                console.log(`${currentPlayer.getPlayer().playerName}` + " Wins!")
+                return true
+            }else if(board[0][i] === sign && board[1][i] === sign && board[2][i] === sign){
+                console.log(`${currentPlayer.getPlayer().playerName}` + " Wins!")
+                return true
+            }
+
+            else if (board[0][0] === sign && board[1][1] === sign && board[2][2] === sign) { // diagonals (retarded)
+            return true;
+            }
+
+            else if (board[0][2] === sign && board[1][1] === sign && board[2][0] === sign) {
+             console.log(`${currentPlayer.getPlayer().playerName} Wins!`);
+                return true;
+             }   
+            }
+       
+        return false
+        
+        
+    }
+                
     return {
-        renderBoard , move
+        renderBoard , move , checkWin , switchPlayers
     }
     
 })()
 
+// HTML STYLING
+// HTML STYLING
+// HTML STYLING
+// HTML STYLING
+// HTML STYLING
+// HTML STYLING
+// HTML STYLING
+// HTML STYLING
+// HTML STYLING
+// HTML STYLING
+// HTML STYLING
+// HTML STYLING
 
 
+const boxes = document.querySelectorAll(".box");
+const container = document.querySelector(".container")
+
+boxes.forEach((box,index) => box.addEventListener("click", () => {
+    if(game.checkWin()){
+        return true
+    }
+    if(box.hasChildNodes()){
+       return  console.log('nlaicenwck')
+    }else
+    
+    index +=1
+    const circle = document.createElement("div")
+    circle.classList.add("circle")
+    const cross = document.createElement("div")
+    cross.classList.add("cross")
+    cross.textContent = 'cross'
+    circle.textContent = 'circle'
+    if(currentPlayer.getPlayer().playerSign === 'o'){
+        box.appendChild(circle)
+    }
+    else box.appendChild(cross)
+    const result = game.move(index)
+    console.log(currentPlayer.getPlayer().playerSign)
+    
+    console.log(result)
+    
+    
+    
+        
+    
+    
+    
+    
+   
+}))
 
